1925256b198ba48cbbdcf063107b4e9611f5ec6a
x1001000 <x1001000@gmail.com>
2015-12-10 08:28:37 +0800

    7-segment LED

--- display.py
+++ display.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+#GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 segments = (10, 9, 11, 5, 6, 13, 19, 26)
 for segment in segments:
     GPIO.setup(segment, GPIO.OUT)
